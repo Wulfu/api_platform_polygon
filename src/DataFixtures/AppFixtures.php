310cae093d38a606855e0a32d617e0151ef87f0a
mateusz <mateusz@student.edu>
2019-01-06 19:25:32 +0100

    basic fixtures with faker lib

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,31 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\Comment;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * @var Factory
+     */
+    private $faker;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->faker = Factory::create();
+    }
+    
     /**
      * @param ObjectManager $manager
      */
@@ -16,37 +35,48 @@ class AppFixtures extends Fixture
     {
         $this->loadUsers($manager);
         $this->loadBlogPosts($manager);
+        $this->loadComments($manager);
     }
 
     public function loadBlogPosts(ObjectManager $manager)
     {
+        /** @var $user User */
         $user = $this->getReference('user_admin');
-        $blogPost = new BlogPost();
-        $blogPost
-            ->setTitle('First post')
-            ->setPublished(new \DateTime('2018-07-29 12:00:00'))
-            ->setContent("Test first content")
-            ->setAuthor($user)
-            ->setSlug("first-post");
 
-        $manager->persist($blogPost);
+        for($i = 0; $i < 100; $i++) {
+            $blogPost = new BlogPost();
+            $blogPost
+                ->setTitle($this->faker->realText(30))
+                ->setPublished($this->faker->dateTime)
+                ->setContent($this->faker->realText())
+                ->setAuthor($user)
+                ->setSlug($this->faker->slug);
 
-        $blogPost = new BlogPost();
-        $blogPost
-            ->setTitle('Second post')
-            ->setPublished(new \DateTime('2018-07-29 12:00:00'))
-            ->setContent("Test second content")
-            ->setAuthor($user)
-            ->setSlug("second-post");
+            $this->setReference("blog_post_$i", $blogPost);
 
-        $manager->persist($blogPost);
+            $manager->persist($blogPost);
+        }
 
         $manager->flush();
     }
 
     public function loadComments(ObjectManager $manager)
     {
+        /** @var User $user */
+        $user = $this->getReference('user_admin');
 
+        for($i = 0; $i < 100; $i++){
+            for($j = 0; $j < rand(1, 10); $j++) {
+                $comment = new Comment();
+                $comment
+                    ->setContent($this->faker->realText())
+                    ->setPublished($this->faker->dateTimeThisYear)
+                    ->setAuthor($user)
+                    ->setBlogPost($this->getReference("blog_post_$i"));
+                $manager->persist($comment);
+            }
+        }
+        $manager->flush();
     }
 
     public function loadUsers(ObjectManager $manager)
@@ -56,7 +86,10 @@ class AppFixtures extends Fixture
             ->setUsername("Szakal")
             ->setEmail("admin@blog.com")
             ->setName("Mateusz Szamota")
-            ->setPassword('secret123#');
+            ->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'secret123#'
+            ));
 
         $this->addReference('user_admin', $user);
 
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -34,6 +34,12 @@ class Comment
      */
     private $author;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\BlogPost", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $blogPost;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -81,5 +87,18 @@ class Comment
         return $this;
     }
 
+    public function getBlogPost(): BlogPost
+    {
+        return $this->blogPost;
+    }
 
+    /**
+     * @param mixed $blogPost
+     * @return self
+     */
+    public function setBlogPost(BlogPost $blogPost): self
+    {
+        $this->blogPost = $blogPost;
+        return $this;
+    }
 }
