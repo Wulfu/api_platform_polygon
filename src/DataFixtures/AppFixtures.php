df92f16a75a67b363027c3187c2019dfdb797101
mateusz <mateusz@student.edu>
2019-01-06 18:25:24 +0100

    easy admin, some fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -13,12 +14,19 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
         $blogPost = new BlogPost();
         $blogPost
             ->setTitle('First post')
             ->setPublished(new \DateTime('2018-07-29 12:00:00'))
             ->setContent("Test first content")
-            ->setAuthor("mateusz szamota")
+            ->setAuthor($user)
             ->setSlug("first-post");
 
         $manager->persist($blogPost);
@@ -28,11 +36,31 @@ class AppFixtures extends Fixture
             ->setTitle('Second post')
             ->setPublished(new \DateTime('2018-07-29 12:00:00'))
             ->setContent("Test second content")
-            ->setAuthor("mateusz szamota")
+            ->setAuthor($user)
             ->setSlug("second-post");
 
         $manager->persist($blogPost);
 
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user
+            ->setUsername("Szakal")
+            ->setEmail("admin@blog.com")
+            ->setName("Mateusz Szamota")
+            ->setPassword('secret123#');
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
